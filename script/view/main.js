7b271477233d086e078b29fcd2ecc2579b5eb15d
Gerry-Geraldi <whateveritakes10@gmail.com>
2023-04-26 05:08:53 +0800

    Add File

---
+++ script/view/main.js
@@ -0,0 +1,28 @@
+import '../component/club-list.js'
+import "../component/search-bar.js";
+import DataSource from "../data/data-source.js";
+const main = () => {
+  const searchElement = document.querySelector("search-bar");
+  const clubListElement = document.querySelector("club-list");
+
+  const onButtonSearchClicked = async () => {
+    try {
+      const results = await DataSource.searchClub(searchElement.value);
+      renderResult(results);
+    } catch (message) {
+      fallbackResult(message);
+    }
+  };
+
+  const renderResult = (results) => {
+    clubListElement.clubs = results;
+  };
+
+  const fallbackResult = message => {
+    clubListElement.renderError(message)
+  }
+
+  searchElement.clickEvent = onButtonSearchClicked;
+};
+
+export default main;
